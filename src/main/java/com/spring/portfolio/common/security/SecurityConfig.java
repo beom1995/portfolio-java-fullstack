5fd91613ddf3afe4505c38f0370188dbd2df42d9
yeosujin <ysjin9992@gmail.com>
2024-06-23 22:54:11 +0900

    # Fix : api auth

--- src/main/java/com/spring/portfolio/common/security/SecurityConfig.java
+++ src/main/java/com/spring/portfolio/common/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtTokenProvider jwtTokenProvider;
@@ -36,7 +38,7 @@ public class SecurityConfig {
             .cors().and()
             .csrf().disable()
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/api/signup", "/api/login").permitAll() // 회원가입, 로그인 API 권한 허용
+                .requestMatchers("/api/**").permitAll() // API 권한 허용
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
