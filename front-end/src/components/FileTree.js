d9cdb5f9769e51cabda2cd0d9a2f2e6f465fa6a4
HYH <dihexyldecyl@gmail.com>
2024-06-23 14:27:04 +0900

    #Feat: FileTree 수정

--- front-end/src/components/FileTree.js
+++ front-end/src/components/FileTree.js
@@ -1,47 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { saveAs } from 'file-saver';
 
 export default function FileTree({ projectId }) {
     const [files, setFiles] = useState([]);
 
+    const fetchFiles = async () => {
+        
+        await axios.get(`/api/project/${projectId}/files`)
+        .then(response => {
+            setFiles(response.data);
+        })
+        .catch(error => {
+            console.error('Failed to fetch files:', error);
+        });
+    };
+    
     useEffect(() => {
         fetchFiles();
-    // }, [projectId]);
     }, []);
 
-    const fetchFiles = async () => {
-        try {
-            const response = await axios.get(`/api/project/${projectId}/files`);
-            setFiles(response.data);
-        } catch (error) {
-            console.error('Failed to fetch files:', error);
-        }
+    const handleFileDownload = async (path) => {
+
+        await axios.get(`/api/project/${projectId}/download`, {
+            params: { filePath: path },
+            responseType: 'blob',
+        })
+        .then(response => {
+            console.log('download success');
+
+            // 서버에서 Content-Disposition 헤더를 가져와 파일 이름 추출
+            const contentDisposition = response.headers['content-disposition'];
+            let fileName = 'downloadedFile';
+            if (contentDisposition) {
+                const matches = /filename\*?=['"]?([^'";]+)['"]?/.exec(contentDisposition);
+                if (matches != null && matches[1]) { 
+                    fileName = matches[1];
+                    console.log(fileName);
+                }
+            }
+
+            // FileSaver.js를 사용하여 파일 저장
+            saveAs(new Blob([response.data]), fileName);
+
+        })
+        .catch(error => {
+            console.error('Failed to download:', error);
+
+        });
     };
 
-    const handleFileDownload = async (fileId) => {
-        try {
-            const response = await axios.get(`/api/project/${projectId}/${fileId}`, {
-                responseType: 'blob'
-            });
-        } catch (error) {
-            console.error('Failed to download:', error);
-        }
+    const removeFile = async (fileId) => {
+        console.log(fileId);
+
+        await axios.delete(`/api/project/${projectId}/${fileId}`)
+        .then(response => {
+            console.log('remove success');
+            fetchFiles();
+        })
+        .catch(error => {
+            console.error('Failed to remove:', error);
+
+        });
     };
 
     const renderTree = (nodes) => (
         <ul>
-        {nodes.map((node) => (
-            <li key={node.path}>
-            {node.type === 'directory' ? (
-                <>
-                <span>{node.name}</span>
-                {renderTree(node.children)}
-                </>
-            ) : (
-                <span onClick={() => handleFileDownload(1)}>{node.name}</span>
-            )}
-            </li>
-        ))}
+            {nodes.map((node) => (
+                <li key={node.path}>
+                    {node.type === 'directory' ? (
+                        <>
+                        <span>{node.name}</span>
+                        {renderTree(node.children)}
+                        </>
+                    ) : (
+                        <>
+                            <span onClick={() => handleFileDownload(node.path)}>{node.name}</span>
+                            <button onClick={() => removeFile(node.fileId)} style={{ marginLeft: '10px', color: 'red' }}>X</button>
+                        </>
+                    )}
+                    
+                </li>
+            ))}
         </ul>
     );
 
