2a03a01ec0dfe093138bb34705e1b70805a860a8
yeosujin <ysjin9992@gmail.com>
2024-06-24 14:03:01 +0900

    # Fix : auth delete

---
+++ src/main/java/com/spring/portfolio/common/aspect/ProjectOwnerOnly.java
@@ -0,0 +1,11 @@
+package com.spring.portfolio.common.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProjectOwnerOnly {
+}
